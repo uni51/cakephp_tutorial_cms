98d1c37911fdde2f659f8c0f90821a97eac79778
Koichi.Shimomura <unieye51@gmail.com>
2021-12-22 09:42:09 +0900

    シンプルなスラグ生成の追加

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -3,6 +3,10 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// Text クラス
+use Cake\Utility\Text;
+// EventInterface クラス
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
@@ -10,5 +14,14 @@ class ArticlesTable extends Table
     {
         // created や modified カラムを自動的に更新する
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // スラグをスキーマで定義されている最大長に調整
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
     }
 }
