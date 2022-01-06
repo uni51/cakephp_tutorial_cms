a1672d6b7fe410893f452dbb8d8c7359f5e0e002
Koichi.Shimomura <unieye51@gmail.com>
2022-01-06 09:08:46 +0900

    タグによる記事の検索

--- config/routes.php
+++ config/routes.php
@@ -57,6 +57,13 @@ $routes->scope('/', function (RouteBuilder $builder) {
      */
     $builder->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
+    // タグ付けられたアクションのために追加された新しいルート。
+    // 末尾の `*` は、このアクションがパラメーターを渡されることを
+    // CakePHP に伝えます。
+    $builder->scope('/articles', function (RouteBuilder $builder) {
+        $builder->connect('/tagged/*', ['controller' => 'Articles', 'action' => 'tags']);
+    });
+
     /*
      * Connect catchall routes for all controllers.
      *
--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -78,4 +78,23 @@ class ArticlesController extends AppController
             return $this->redirect(['action' => 'index']);
         }
     }
+
+    public function tags()
+    {
+        // 'pass' キーは CakePHP によって提供され、リクエストに渡された
+        // 全ての URL パスセグメントを含みます。
+        $tags = $this->request->getParam('pass');
+
+        // ArticlesTable を使用してタグ付きの記事を検索します。
+        $articles = $this->Articles->find('tagged', [
+            'tags' => $tags
+        ])
+            ->all();
+
+        // 変数をビューテンプレートのコンテキストに渡します。
+        $this->set([
+            'articles' => $articles,
+            'tags' => $tags
+        ]);
+    }
 }
